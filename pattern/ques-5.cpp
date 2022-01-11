69443bb763cb137e39ca631dfd7ebde4fd4c70ae
Shreeja Raj <61755604+shreeja26@users.noreply.github.com>
2022-01-11 22:32:54 +0530

    Create ques-5.cpp

---
+++ pattern/ques-5.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+using namespace std;
+int main() {
+    cout << "\n";
+    int n;
+    cin >> n;
+    for(int i=0; i<n; i++){
+        for(int j=n-1; j>i; j--){
+            cout << " ";
+        }
+        for(int j=0; j<=i; j++){
+            cout << "* ";
+        }
+        cout << "\n";
+    }
+    for(int i=0; i<n; i++){
+        for(int j=0; j<=i; j++){
+            cout << " ";
+        }
+        for(int j=n-1; j>i; j--){
+            cout <<  "* ";
+        }
+        cout << "\n";
+    }
+    return 0;
+}
