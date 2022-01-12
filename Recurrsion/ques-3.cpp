f81eeb282bc31c9fc081d9bed6479c15172925d7
Shreeja Raj <61755604+shreeja26@users.noreply.github.com>
2022-01-13 00:05:57 +0530

    Create ques-3.cpp

---
+++ Recurrsion/ques-3.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+void natural(int n){
+    if(n<=0)
+        return ;
+    natural(n-1);
+    if(n%2==0)
+        cout << n<< " ";
+}
+
+int main() {
+    int n;
+    cin >> n;
+    natural(n);
+    return 0;
+}
